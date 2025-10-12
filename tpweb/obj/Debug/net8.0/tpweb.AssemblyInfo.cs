b0e725f19e9395afe4ae15882fec8f180c8c8fbd
Corchitobj <san.tropea@gmail.com>
2025-10-12 17:08:12 -0300

    Cambios

--- tpweb/obj/Debug/net8.0/tpweb.AssemblyInfo.cs
+++ tpweb/obj/Debug/net8.0/tpweb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpweb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3430bd7d41e71a2c94239460a8a00b7fe73c6a69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddc3d4b71366117a93cc9252fd7519ce3d2b4fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpweb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpweb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
